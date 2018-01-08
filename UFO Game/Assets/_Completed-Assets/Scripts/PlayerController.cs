06c1776c10d6e5ec033d156775cd31a799cb4756
ducthong230497 <15520856@gm.uit.edu.vn>
2018-01-08 09:47:45 +0700

    Initial commit

---
+++ UFO Game/Assets/_Completed-Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb2D;
+    public Text countText;
+    public Text winText;
+    private int count;
+	// Use this for initialization
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText(count);
+        winText.text = string.Empty;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2D.AddForce(movement * speed);
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            SetCountText(++count);
+            if (count == 10)
+            {
+                winText.text = "You win!";
+            }
+        }
+    }
+
+    void SetCountText(int count)
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+}
---
+++ UFO Game/Assets/_Completed-Assets/Scripts/Rotater.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 rotate = new Vector3(0, 0, 30);
+        transform.Rotate(rotate * Time.deltaTime);
+	}
+}
